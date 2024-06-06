3118c1589cfd7237ddcc295f3f642ae4151453d4
shayanjanjua <110618809+shayanjanjua@users.noreply.github.com>
2024-06-07 04:58:49 +0500

    Update Melanoma.py

--- Melanoma.py
+++ Melanoma.py
@@ -59,7 +59,7 @@ def app():
     """, unsafe_allow_html=True)
 
     # Add the image after the treatment section
-    image_path = r'C:\Users\Shayan\Desktop\FYP\DEEP DERMA APP\IMAGES\Melanoma.png' # Replace with your image path
+    image_path = r'IMAGES/Melanoma.png' # Replace with your image path
     image = Image.open(image_path)
     st.image(image, caption='Example of Melanoma')       
             
