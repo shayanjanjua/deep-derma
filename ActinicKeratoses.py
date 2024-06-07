066143d75cbf2f0d11a1d1eac5371d9d2d5067de
shayanjanjua <110618809+shayanjanjua@users.noreply.github.com>
2024-06-07 05:07:10 +0500

    Update ActinicKeratoses.py

--- ActinicKeratoses.py
+++ ActinicKeratoses.py
@@ -56,7 +56,7 @@ def app():
     """, unsafe_allow_html=True)
 
     # Add the image after the treatment section
-    image_path = r'IMAGES/ACtinic Keratoses.png' # Replace with your image path
+    image_path = r'IMAGES/ACtinic Keratoses .png' # Replace with your image path
     image = Image.open(image_path)
     st.image(image, caption='Example of Actinic Keratoses Treatment')    
 
